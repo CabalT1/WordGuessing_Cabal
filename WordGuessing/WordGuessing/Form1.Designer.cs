632e808112ebaa61a2768f75771ceb5bd60fb527
CabalVolletail <johnalbertcabal@gmail.com>
2025-08-26 22:41:09 +0800

    Add files via upload

---
+++ WordGuessing/WordGuessing/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WordGuessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtGuess = new TextBox();
+            btnGuess = new Button();
+            lstWrongGuesses = new ListBox();
+            label1 = new Label();
+            lblWord = new Label();
+            SuspendLayout();
+            // 
+            // txtGuess
+            // 
+            txtGuess.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtGuess.Location = new Point(196, 221);
+            txtGuess.Name = "txtGuess";
+            txtGuess.Size = new Size(268, 33);
+            txtGuess.TabIndex = 0;
+            // 
+            // btnGuess
+            // 
+            btnGuess.BackColor = Color.DarkSeaGreen;
+            btnGuess.FlatStyle = FlatStyle.Flat;
+            btnGuess.Font = new Font("Segoe UI", 12F);
+            btnGuess.ForeColor = Color.White;
+            btnGuess.Location = new Point(295, 260);
+            btnGuess.Name = "btnGuess";
+            btnGuess.Size = new Size(66, 34);
+            btnGuess.TabIndex = 1;
+            btnGuess.Text = "Guess";
+            btnGuess.UseVisualStyleBackColor = false;
+            btnGuess.Click += button1_Click;
+            // 
+            // lstWrongGuesses
+            // 
+            lstWrongGuesses.FormattingEnabled = true;
+            lstWrongGuesses.ItemHeight = 15;
+            lstWrongGuesses.Location = new Point(532, 136);
+            lstWrongGuesses.Name = "lstWrongGuesses";
+            lstWrongGuesses.Size = new Size(186, 184);
+            lstWrongGuesses.TabIndex = 2;
+            lstWrongGuesses.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ButtonShadow;
+            label1.Location = new Point(592, 118);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Wrong Guess";
+            // 
+            // lblWord
+            // 
+            lblWord.AutoSize = true;
+            lblWord.BackColor = Color.White;
+            lblWord.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblWord.Location = new Point(265, 182);
+            lblWord.Name = "lblWord";
+            lblWord.Size = new Size(0, 21);
+            lblWord.TabIndex = 4;
+            lblWord.Click += lblWord_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(128, 255, 255);
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblWord);
+            Controls.Add(label1);
+            Controls.Add(lstWrongGuesses);
+            Controls.Add(btnGuess);
+            Controls.Add(txtGuess);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtGuess;
+        private Button btnGuess;
+        private ListBox lstWrongGuesses;
+        private Label label1;
+        private Label lblWord;
+    }
+}
---
+++ WordGuessing/WordGuessing/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WordGuessing
+{
+    public partial class Form1 : Form
+    {
+        // 🔹 Declare variables as class fields
+        private string wordToGuess = "computer";   // The word to guess
+        private StringBuilder maskedWord;          // To hold hidden word
+        private ArrayList wrongGuesses;            // Store wrong guesses
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            wrongGuesses = new ArrayList();
+            maskedWord = new StringBuilder();
+
+
+            maskedWord.Append(wordToGuess[0]); // first letter
+            for (int i = 1; i < wordToGuess.Length - 1; i++)
+                maskedWord.Append("?");
+            maskedWord.Append(wordToGuess[wordToGuess.Length - 1]); // last letter
+
+            lblWord.Text = maskedWord.ToString();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userGuess = txtGuess.Text.Trim().ToLower();
+
+            if (string.IsNullOrEmpty(userGuess))
+            {
+                MessageBox.Show("Please enter a guess.");
+                return;
+            }
+
+            if (userGuess == wordToGuess)
+            {
+                lblWord.Text = wordToGuess; // reveal answer
+                MessageBox.Show("Correct! The word is " + wordToGuess);
+                txtGuess.Clear();
+                return;
+            }
+            else
+            {
+                if (!wrongGuesses.Contains(userGuess))
+                {
+                    wrongGuesses.Add(userGuess);
+                    lstWrongGuesses.Items.Add(userGuess); // display in listbox
+                }
+                else
+                {
+                    MessageBox.Show("You already tried that word!");
+                }
+            }
+
+            txtGuess.Clear();
+            txtGuess.Focus();
+        }
+
+        private void lblWord_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
